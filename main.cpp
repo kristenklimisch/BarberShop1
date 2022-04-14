8488eac68dea7d60401be7df45e894f5760f3f87
kristenklimisch <kristen.klimisch@gmail.com>
2022-04-13 23:14:04 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -29,8 +29,10 @@ sem_t barberChair;
 // a customer wakes him. 
 sem_t wakeBarber;
 
-// Flag indicating barber's work is done for the day. 
-bool done_with_all_customers = FALSE; 
+// Flag used to indicate when the barber's work is done for the day.
+// Initialize to 0. When value changes to 1, barber is done for the
+// day. 
+bool done_with_all_customers = 0; 
 
 // Global variable for number of customers in the barbershop.
 int customersInShop = 0;
@@ -48,12 +50,14 @@ int main (int argc, char *argv[]) {
 
     // Accept user input for the number of chairs in the waiting room.
     printf("Enter the number of chairs in barbershop waiting room: ");
-    scanf("%d", &n);
+    check = scanf("%d", &n);
+    printf("n is %d", n);
 
     // Perform error checking for user input. 
-    if ((check != 1) || (n < 0)){
-        printf("Invalid entry - number of chairs must be a positive number."\n
-        "Program exiting."\n);
+   // if ((check != 1) || (n < 0)) {
+    if (n < 0) {
+        printf("Invalid entry - number of chairs must be a positive number.\n");
+        printf("Program exiting.\n");
         exit(-1);
     }
 
@@ -71,7 +75,7 @@ int main (int argc, char *argv[]) {
     // Create the customer threads.
     int customerID[numCustomers];
     for (i = 0; i<numCustomers; i++) {
-        customer_ID[i] = i;
+        customerID[i] = i +1;
         pthread_create(&tid[i], NULL, customer, &customerID[i]);
         sleep(1); 
     }
@@ -81,12 +85,14 @@ int main (int argc, char *argv[]) {
         pthread_join(tid[i], 0);
     } 
 
-    done_with_all_customers = TRUE;
+    done_with_all_customers = 1;
 
     // Wake the barber so he will know he can exit. 
     sem_post(&wakeBarber); 
 
     pthread_join(btid, NULL);
+
+    printf("Barber is leaving for the day.");
 }
 
 void *barber(void *arg) {
@@ -94,9 +100,14 @@ void *barber(void *arg) {
         
         // When there are no customers in the waiting room or
         // getting their hair cut, barber sleeps in the barber chair. 
-        while (customersInShop == 0) {
+        // KK - for some reason, this repeats every 4 customers.
+
+        pthread_mutex_lock(&customers);
+        if (customersInShop == 0) {
             printf("Barber is sleeping in barber chair.\n");
         }
+        pthread_mutex_unlock(&customers);
+
         
         // Barber sleeps until he is woken up by a customer.
         sem_wait(&wakeBarber);
@@ -109,6 +120,7 @@ void *barber(void *arg) {
 
         // Sleep to simulate haircut. In this simple program,
         // all haircuts take the same amount of time. 
+        // KK need to update this to generate random number.
         sleep(5);
 
     }
@@ -116,12 +128,12 @@ void *barber(void *arg) {
 
 void *customer(void *customerNumber) {
     int number = *(int *)customerNumber;
-    printf("Customer %d attempting to enter barber shop.\n");
+    printf("Customer %d attempting to enter barber shop.\n", number);
 
     // Wait until a chair is available in the waiting room 
     // to enter the waiting room. 
     sem_wait(&chairsAvailable);
-    printf("Customer %d entering waiting room.\n");
+    printf("Customer %d entering waiting room.\n", number);
 
     // Increment number of customers in barber shop when a customer
     // enters the waiting room. 
@@ -139,28 +151,17 @@ void *customer(void *customerNumber) {
     // chair in waiting room. 
     sem_post(&chairsAvailable);
 
-    printf("Customer %d getting hair cut.\n");
+    printf("Customer %d getting hair cut.\n", number);
 
     // After haircut is complete, customer gives up the barber chair.
     sem_post(&barberChair);
-    printf("Customer %d done with haircut and exiting barbershop.\n");
+    printf("Customer %d done with haircut and exiting barbershop.\n", number);
 
     // Decrement number of customers in barber shop when a customer
     // completes their haricut and exits barbershop.  
     pthread_mutex_lock(&customers);
     customersInShop--;
     pthread_mutex_unlock(&customers);
-}
-
-
-
-    
-
-
-
-
-
-
 }
 
 
